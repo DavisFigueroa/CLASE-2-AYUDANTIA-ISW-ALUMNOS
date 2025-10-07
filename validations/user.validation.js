714b84e1a910e8acc06d5e67d30e2b6ec8478ed7
DavisFigueroa <davis.figueroa2201@alumnos.ubiobio.cl>
2025-10-07 17:46:08 -0300

    Mejora validaciones

--- validations/user.validation.js
+++ validations/user.validation.js
@@ -1,6 +1,12 @@
 import Joi from 'joi';
 export const userBodyValidation = Joi.object({
 
-  email: Joi.string().min(3).max(50).required(),
-  password: Joi.string().min(3).max(20).required()
-});
+  email: Joi.string().min(3).max(50).pattern(/^[A-Za-z0-9.]+@[A-Za-z0-9.]+$/).required()
+  .messages({ "string.pattern.base": "El Email solo puede contener numeros, letras y el @"}),
+  
+  password: Joi.string().min(3).max(30).required().messages({ "string.min": "La contraseña es demasiado corta, debe tener mínimo 8 caracteres"})
+}).options({
+  allowUnknown: false,
+  stripUnknown: true,
+  abortEarly: false
+  });
