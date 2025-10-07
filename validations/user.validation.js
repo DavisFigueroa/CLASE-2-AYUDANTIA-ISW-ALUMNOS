6b4302f221d907a60aab15d6c033943c51558a60
DavisFigueroa <davis.figueroa2201@alumnos.ubiobio.cl>
2025-10-07 17:48:02 -0300

    Mejoras validación
    Olvide agregar algo

--- validations/user.validation.js
+++ validations/user.validation.js
@@ -1,10 +1,10 @@
 import Joi from 'joi';
 export const userBodyValidation = Joi.object({
 
-  email: Joi.string().min(3).max(50).pattern(/^[A-Za-z0-9.]+@[A-Za-z0-9.]+$/).required()
+  email: Joi.string().min(8).max(50).pattern(/^[A-Za-z0-9.]+@[A-Za-z0-9.]+$/).required()
   .messages({ "string.pattern.base": "El Email solo puede contener numeros, letras y el @"}),
-  
-  password: Joi.string().min(3).max(30).required().messages({ "string.min": "La contraseña es demasiado corta, debe tener mínimo 8 caracteres"})
+
+  password: Joi.string().min(8).max(30).required().messages({ "string.min": "La contraseña es demasiado corta, debe tener mínimo 8 caracteres"})
 }).options({
   allowUnknown: false,
   stripUnknown: true,
